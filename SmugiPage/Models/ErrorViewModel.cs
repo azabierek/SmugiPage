951d5029437cb0a6e97043e71bb8b1f1e2111de5
unknown <adszbrk@o2.pl>
2022-04-08 20:20:40 +0200

    Pierwszy commit, stworzenie pustego projektu

---
+++ SmugiPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmugiPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
